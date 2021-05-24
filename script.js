f08de723966e0bcfa2d89883e0b34c6e19ebe0dd
Ace E Emel <acasualmemelord.com>
2021-05-24 16:46:39 -0700

    f

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function importHTML(){
   var paste = document.getElementById("paste").value;
-  document.getElementById("test").value = paste.substring(paste.indexOf("|poke|p1|"))
+  try{
+    document.getElementById("test1").innerHTML = paste.indexOf("|poke|p1|") + " " + paste.substring(paste.indexOf("|poke|p1|") + 2).indexOf("|poke|p1|");
+    document.getElementById("test").innerHTML = paste.substring(paste.indexOf("|poke|p1|"), paste.substring(paste.indexOf("|poke|p1|") + 1).indexOf("|poke|p1|"));
+  }catch(err){
+    document.getElementById("test").innerHTML = err;
+  }
 }
